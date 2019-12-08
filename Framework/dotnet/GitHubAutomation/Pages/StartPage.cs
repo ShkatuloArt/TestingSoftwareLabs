2a1c8029d336f09d09fe17c84f51a0e1736efdfa
ShkatuloArtem <shkatulo.2017@gmail.com>
2019-12-08 18:56:19 +0300

    Framework

---
+++ Framework/dotnet/GitHubAutomation/Pages/StartPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using GitHubAutomation.Model;
+using GitHubAutomation.Service;
+using GitHubAutomation.Driver;
+
+namespace GitHubAutomation.Pages
+{
+    public class StartPage
+    {
+        public StartPage(IWebDriver webDriver)
+        {
+            PageFactory.InitElements(webDriver, this);
+        }
+        [FindsBy(How = How.Id, Using = "select-member")]
+        private IWebElement signInButton;
+
+        [FindsBy(How = How.Id, Using = "MemberId")]
+        private IWebElement memberIdInput;
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        private IWebElement passwordInput;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='col-mb-6']/button[@class='button btn btn-b2-login ui-corner-all']")]
+        private IWebElement enterButton;
+
+        [FindsBy(How = How.XPath, Using = "//div[@id='navtab']/ul[@class='nav-tabs']/li[3]/a")]
+        private IWebElement reservationStatusTab;      
+
+        public StartPage ClickSignInAccountButton(IWebDriver webDriver)
+        {
+            signInButton.Click();
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(10))
+                .Until(ExpectedConditions.ElementToBeClickable(enterButton));
+            return this;
+        }
+
+        public StartPage FillInLoginAndPassword(SignIn signIn)
+        {
+            memberIdInput.SendKeys(signIn.MemberId);          
+            passwordInput.SendKeys(signIn.Password);
+            enterButton.Click();
+            return this;
+        }
+
+        public StartPage GoToTabReservation(IWebDriver webDriver)
+        {
+            reservationStatusTab.Click();
+            return this;
+        }        
+    }
+}
---
+++ Framework/dotnet/GitHubAutomation/Tests/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+using GitHubAutomation.Driver;
+using GitHubAutomation.Service;
+using GitHubAutomation.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.Extensions;
+
+namespace GitHubAutomation.Tests
+{
+    [TestFixture]
+    public class Tests : TestConfig
+    {
+        [Test]
+        public void SignInToAccount()
+        {
+            TakeScreenshotWhenTestFailed(() =>
+            {
+                StartPage startPage = new StartPage(Driver)
+                .ClickSignInAccountButton(Driver)
+                .FillInLoginAndPassword(CreatorSignIn.WithUserProperties());
+                AuthorizedPage authorizedPage = new AuthorizedPage(Driver);
+                Assert.AreEqual("ARTSIOM SHKATULA", authorizedPage.textSignInButton.Text);
+            });
+        }
+
+        [Test]
+        public void InputInvalidReservationCodeWhenViewingReservationStatus()
+        {
+            TakeScreenshotWhenTestFailed(() =>
+            {
+                StartPage startPage = new StartPage(Driver)
+               .GoToTabReservation(Driver);
+                ReservationPage reservationPage = new ReservationPage(Driver)
+                    .FillInReservationCodeAndPassengerName(CreatingReservationModel.WithReservationProperties());
+                ReservationSearchResultsPage reservationSearchResultsPage = new ReservationSearchResultsPage(Driver);
+                Assert.AreEqual("Бронирование не найдено", reservationSearchResultsPage.errorMessage.Text);
+            });           
+        }
+    }
+}
